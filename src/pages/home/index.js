8ddc0dc2db6ad0de891c37396d58f1aff8bcf463
outhsics <outhsics@gmail.com>
2018-04-07 20:32:57 +0800

    修改后台表单

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,24 +2,29 @@ import React from 'react'
 import Header from '../../components/header'
 import {Row,Col} from 'antd'
 import NavLeft from '../../components/navleft'
+import menuList from '../../contones/menuConfig'
 
 export default class Home extends React.Component{
+    state={}
+    componentWillMount(){
+        this.setState({
+            menuList
+        })
+    }
     render (){
         return (
             <div>
-                <div className='welcome-page'>
+                <div>
                     <Header username={'珠峰培训'}/>
-                    <Row>
-                        <Col span={3} className='nav-left'>
-                            <NavLeft/>
+                    <Row className='welcome-page'>
+                        <Col span='3' className='nav-left'>
+                            <NavLeft menuList={this.state.menuList}/>
                         </Col>
-                        <Col span={21} className='right-container'>
+                        <Col span='21' className="right-container">
                             {this.props.children}
                         </Col>
                     </Row>
                 </div>
-                
-                
             </div>
         )
     }
